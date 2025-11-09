8be98d3b4c9b1adb1e18d314fa5ef2b27284f320
2021wa15539-droid <2021wa15539@wilp.bits-pilani.ac.in>
2025-11-09 21:57:19 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+  version = "~> 20.24"   # latest stable release
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
@@ -21,11 +21,9 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = ["m5.large"]
-
     attach_cluster_primary_security_group = true
   }
 
